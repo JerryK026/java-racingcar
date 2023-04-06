689cf83a5e61f6ebaa1618eefd836754cc79ff78
SeokyungKim <kskyung0624@gmail.com>
2023-04-06 16:19:27 +0900

    refactor : split 사용시 매번 작업을 요청하지 않도록 캐시를 사용한다

--- src/main/java/calculator/AdditionalString.java
+++ src/main/java/calculator/AdditionalString.java
@@ -1,21 +1,31 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AdditionalString {
   private static final String DEFAULT_DELIMITER = ",|:";
   private static final String EMPTY = "";
+  private static final Integer HEAD_INDEX = 0;
   private static final Integer DELIMITER_INDEX = 1;
   private static final Integer CONTENT_INDEX = 2;
 
   private final String value;
+  private List<String[]> cache;
 
   AdditionalString(String value) {
     this.value = emptyIfNull(value);
+    this.cache = new ArrayList<>();
   }
 
   public String[] split() {
+    if (!cache.isEmpty()) {
+      return cache.get(HEAD_INDEX);
+    }
+
     if (this.value.isEmpty()) {
       return new String[]{};
     }
@@ -29,10 +39,18 @@ public class AdditionalString {
       target = matcher.group(this.CONTENT_INDEX);
     }
 
-    return target.split(delimiter);
+    String[] result = target.split(delimiter);
+    putCache(result);
+
+    return result;
   }
 
   private String emptyIfNull(String value) {
     return value == null ? this.EMPTY : value;
+  }
+
+  private void putCache(String[] value) {
+    this.cache.add(value);
+    Collections.unmodifiableList(this.cache);
   }
 }
